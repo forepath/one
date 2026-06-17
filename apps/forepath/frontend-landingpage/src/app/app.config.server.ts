a8249e31f49e66804c936c05fc737c66a43908e7
Marcel <74356027+me-marcel@users.noreply.github.com>
2026-06-17 16:45:37 +0200

    fix: forepath landingpage delegation server (#423)

--- apps/forepath/frontend-landingpage/src/app/app.config.server.ts
+++ apps/forepath/frontend-landingpage/src/app/app.config.server.ts
@@ -1,12 +1,11 @@
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { provideServerRendering } from '@angular/platform-server';
 import { ENVIRONMENT, environment } from '@forepath/shared/frontend/util-configuration';
 
 import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(withRoutes(serverRoutes)), { provide: ENVIRONMENT, useValue: environment }],
+  providers: [provideServerRendering(), { provide: ENVIRONMENT, useValue: environment }],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
