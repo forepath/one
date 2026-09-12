89f33aee7cb86d53d8a5d8adcb23faca4c2d1e8e
Marcel <74356027+me-marcel@users.noreply.github.com>
2026-09-12 12:10:33 +0200

    feat(shared): landingpage portal links (#611)

--- libs/domains/decabill/frontend/feature-landingpage/src/lib/container/container.component.ts
+++ libs/domains/decabill/frontend/feature-landingpage/src/lib/container/container.component.ts
@@ -40,6 +40,10 @@ export class PortalContainerComponent {
   private readonly locale = inject(LOCALE_ID);
   private readonly router = inject(Router);
 
+  readonly portalUrl = this.environment.production
+    ? `${this.environment.billing.frontendUrl}/${this.locale}`
+    : this.environment.billing.frontendUrl;
+
   readonly withdrawalUrl = this.environment.production
     ? `${this.environment.billing.frontendUrl}/${this.locale}/withdrawal`
     : `${this.environment.billing.frontendUrl}/withdrawal`;
